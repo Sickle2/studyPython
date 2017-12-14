03494cb33fbc96afb58e5ff99acc732097a3008b
Sickle1 <zhaoshuai@ucfgroup.com>
2017-12-14 11:54:03 +0800

    study tset

--- forTest.py
+++ forTest.py
@@ -9,3 +9,59 @@ for fruit in fruits:  # 第二个实例
     print '当前水果 :', fruit
 
 print "Good bye!"
+
+# continue 和 break 用法
+
+i = 1
+while i < 10:
+    i += 1
+    if i % 2 > 0:  # 非双数时跳过输出
+        continue
+    print i  # 输出双数2、4、6、8、10
+
+i = 1
+while 1:  # 循环条件为1必定成立
+    print i  # 输出1~10
+    i += 1
+    if i > 10:  # 当i大于10时跳出循环
+        break;
+
+#素数
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+i = 2
+while (i < 100):
+    j = 2
+    while (j <= (i / j)):
+        if not (i % j): break
+        j = j + 1
+    if (j > i / j): print i, " 是素数"
+    i = i + 1
+
+print "Good bye!"
+
+
+#求质数
+num=[];
+i = 2;
+for i in range(2,100):
+    j = 2;
+    for j in range(2,i):
+        if(i%j==0):
+         break
+    else:
+        num.append(i);
+
+print num;
+
+num=[];
+i=2
+for i in range(2,100):
+   j=2
+   for j in range(2,i):
+      if(i%j==0):
+         break
+   else:
+      num.append(i)
+print(num+list('sss'))
